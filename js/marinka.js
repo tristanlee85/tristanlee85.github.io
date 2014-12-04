5764bb6a3578321614a1e686b4f8c6de26609ac4
Tristan Lee <tristanlee85@gmail.com>
2014-12-04 15:22:27 -0500

    Update marinka.js

--- js/marinka.js
+++ js/marinka.js
@@ -7,7 +7,7 @@ jQuery(document).ready(function() {
     jQuery('.skillbar').each(function() {
         jQuery(this).find('.skillbar-bar').animate({
             width: jQuery(this).attr('data-percent')
-        }, 3000);
+        }, 2000);
     });
 });
 
